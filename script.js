11b703e015b0e021c6a7b9916d560c7851d697f8
AugustoMagalhaes <augustommg@gmail.com>
2021-12-23 01:38:50 -0300

    Adicionando resolucao dos requisitos 7 e 8.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const taskList = document.getElementById('lista-tarefas');
 const createTaskBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
+const taskListItems = document.getElementsByClassName('item-tarefas');
 
 function addTask() {
   const newTaskListElement = document.createElement('li');
   newTaskListElement.className = 'item-tarefas';
   newTaskListElement.innerText = inputTask.value;
+  newTaskListElement.addEventListener('click', selectTask);
 
   try {
     if (inputTask.value.length === 0) throw 'Digite uma tarefa válida';
@@ -17,7 +19,7 @@ function addTask() {
   }
   taskList.appendChild(newTaskListElement);
   inputTask.value = '';
-  const taskListItems = document.getElementsByClassName('item-tarefas');
+  
 }
 
 createTaskBtn.addEventListener('click', addTask);
@@ -26,4 +28,17 @@ inputTask.addEventListener('keypress', function(event) {
     addTask();
   }
 });
+
+function selectTask(event) {
+  const taskElement = event.target;
+  const taskElementColor = window.getComputedStyle(taskElement, null).getPropertyValue('background-color');
+  taskElement.style.backgroundColor = (taskElementColor === 'rgb(128, 128, 128)') ? 'rgba(0, 0, 0, 0)' : 'rgb(128, 128, 128';
+  for (let task of taskListItems) {
+    if (task !== event.target) {
+      task.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    }
+  }
+}
+
+
 
