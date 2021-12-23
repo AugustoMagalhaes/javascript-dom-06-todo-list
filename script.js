5fb2018ad0646f4aa6553772246763b5b3118dc3
AugustoMagalhaes <augustommg@gmail.com>
2021-12-23 00:48:43 -0300

    Alterando a funcao addTask para evitar erros de input vazios ou exclusivamente numericos.

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ function addTask() {
   const newTaskListElement = document.createElement('li');
   newTaskListElement.className = 'item-tarefas';
   newTaskListElement.innerText = inputTask.value;
+
+  try {
+    if (inputTask.value.length === 0) throw 'Digite uma tarefa válida';
+    if (parseFloat(inputTask.value) == inputTask.value) throw 'Digite uma tarefa como texto';
+  } catch (error) {
+    alert(error);
+    inputTask.value = '';
+    return;
+  }
   taskList.appendChild(newTaskListElement);
   inputTask.value = '';
+  const taskListItems = document.getElementsByClassName('item-tarefas');
 }
 
 createTaskBtn.addEventListener('click', addTask);
