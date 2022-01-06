f371de4d23c96038ca05dcd16a08209a5158f435
AugustoMagalhaes <augustommg@gmail.com>
2022-01-06 01:16:48 -0300

    R12 - Criando a funcao 'saveList' e adcionando-a ao botao 'salvar lista'; criando um botao extra 'apagar lista salva' que apaga o conteudo de localStorage.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const createTaskBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 //const taskListItems = document.getElementsByClassName('item-tarefas');
 const finishedTasksList = document.getElementsByClassName('completed');
+const moveUpBtn = document.getElementById('mover-cima');
+const moveDownBtn = document.getElementById('mover-baixo');
 
 function addTask() {
   const newTaskListElement = document.createElement('li');
@@ -33,25 +35,17 @@ inputTask.addEventListener('keypress', function (event) {
 function selectTask(event) {
   const actualSelected = taskList.getElementsByClassName('selected');
   const taskElement = event.target;
-  /* if (taskElement.id === 'selected') {
-    taskElement.removeAttribute('id', 'selected');
-  } else {
-    taskElement.setAttribute('id', 'selected');
-  }
-  for (const task of taskListItems) {
-    if (task !== event.target) {
-      task.removeAttribute('id', 'selected');
-    }
-  } */
-
 
   if (actualSelected[0] && actualSelected[0] !== taskElement) {
     actualSelected[0].classList.remove('selected');
     taskElement.classList.add('selected');
+  } else if (event.target === moveDownBtn || event.target === moveUpBtn) {
+    actualSelected.classList.remove('selected');
   } else {
     taskElement.classList.toggle('selected');
     //actualSelected.classList.remove('selected');    
   }
+
 }
 
 function decorateFinishedTask(event) {
@@ -68,7 +62,7 @@ function decorateFinishedTask(event) {
 }
 
 function clearTaskList() {
-  const taskListItems = document.getElementsByClassName('item-tarefass');
+  const taskListItems = document.getElementsByClassName('item-tarefas');
   for (let index = taskListItems.length - 1; index >= 0; index -= 1) {
     const singleTask = taskListItems[index];
     taskList.removeChild(singleTask);
@@ -88,25 +82,25 @@ function removeFinishedTasks() {
 const removeFinishedTasksBtn = document.getElementById('remover-finalizados');
 removeFinishedTasksBtn.addEventListener('click', removeFinishedTasks);
 
+function saveList() {
+  const taskListContent = taskList.innerHTML;
+  localStorage.setItem('taskList', JSON.stringify(taskListContent));
+  // agradecimento ao @gabsufrrj por ter me explicado o requisito.
+}
+
+const saveListBtn = document.getElementById('salvar-tarefas');
+saveListBtn.addEventListener('click', saveList);
+
 function moveUpTask() {
   if (!taskList.getElementsByClassName('selected')[0]) {
     return;
   }
   const selectedUpItem = taskList.getElementsByClassName('selected');
-  const previousUpItem = selectedUpItem[0].previousElementSibling;  
+  const previousUpItem = selectedUpItem[0].previousElementSibling;
   if (selectedUpItem[0] && previousUpItem) {
     const parent = selectedUpItem[0].parentNode;
     parent.insertBefore(selectedUpItem[0], previousUpItem);
   }
-
-  /* if (document.getElementById('selected') === null) {
-    return null;
-  }
-  const selectedItem = document.getElementById('selected');
-  const previousEl = selectedItem.previousElementSibling;
-  if (previousEl !== null) {
-    selectedItem.parentNode.insertBefore(selectedItem, previousEl);
-  } */
 }
 
 // fonte: https://stackoverflow.com/questions/2698793/swapping-li-elements-with-replacechild-possible
@@ -124,10 +118,10 @@ function moveDownTask() {
   }
 }
 
-const moveUpBtn = document.getElementById('mover-cima');
+//moveup
 moveUpBtn.addEventListener('click', moveUpTask);
 
-const moveDownBtn = document.getElementById('mover-baixo');
+//move down
 moveDownBtn.addEventListener('click', moveDownTask);
 
 function removeSelected() {
@@ -139,3 +133,18 @@ function removeSelected() {
 
 const removeSeletecItemBtn = document.getElementById('remover-selecionado');
 removeSeletecItemBtn.addEventListener('click', removeSelected);
+
+function resetLocalStorage() {
+  localStorage.clear();
+}
+
+const eraseStorageList = document.getElementById('erase-storage');
+eraseStorageList.addEventListener('click', resetLocalStorage);
+
+window.onload = (function() {
+  if (localStorage.length > 0) {
+    const localStorageTaskList = localStorage.getItem('taskList');
+    taskList.innerHTML = JSON.parse(localStorageTaskList);
+  }
+  inputTask.value = '';
+});
